1d826cbea69039a495fdb42dd7b4e02f5395e288
Yegor Kirsanov <kirsanov.yegor@gmail.com>
2019-04-06 17:12:39 +0300

    simple site

--- index.js
+++ index.js
@@ -4,10 +4,29 @@ const fs = require('fs')
 
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
-        res.end('<h1>Home</h1>')
+        fs.readFile(
+            path.join(__dirname, 'public', 'index.html'),
+            (err, content) => {
+                if (err) throw err
+                res.writeHead(200, { 'Content-Type': 'text/html' })
+                res.end(content)
+            },
+        )
     }
-    if (req.url === '/about') {
-        res.end('<h1> About </h1>')
+
+    if (req.url === '/api') {
+        const users = [
+            {
+                name: 'Bob Smith',
+                age: 30,
+            },
+            {
+                name: 'Jek Wilan',
+                age: 40,
+            },
+        ]
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify(users))
     }
 })
 
